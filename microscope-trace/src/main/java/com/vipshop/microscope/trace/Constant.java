cbf1ec25462b1aac8318a9a8c626f2a821f2e8ac
xu fei <andy19881006@gmail.com>
2013-10-17 11:17:51 +0800

    add config for dev qa prod

--- microscope-trace/src/main/java/com/vipshop/microscope/trace/Constant.java
+++ microscope-trace/src/main/java/com/vipshop/microscope/trace/Constant.java
@@ -4,7 +4,7 @@ import com.vipshop.microscope.common.cfg.Configuration;
 
 public class Constant {
 	
-	private static final Configuration config = Configuration.getConfiguration("trace-config.properties");
+	private static final Configuration config = Configuration.getConfiguration("trace.properties");
 	
 	public static final String APP_NAME = config.getString("app_name");
 	
@@ -12,7 +12,7 @@ public class Constant {
 	public static final int COLLECTOR_PORT = config.getInt("collector_port");
 
 	public static final int MAX_BATCH_SIZE = config.getInt("max_batch_size");
-	public static final int MAX_EMPTY_SIZE = config.getInt("max_empty_siez");
+	public static final int MAX_EMPTY_SIZE = config.getInt("max_empty_size");
 
 	public static final int SWITCH = config.getInt("switch");
 
