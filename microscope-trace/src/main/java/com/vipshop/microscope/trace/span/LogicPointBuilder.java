91398e94b661b2cf3a393317c4358c78d85805c8
xu fei <andy19881006@gmail.com>
2013-10-21 16:41:57 +0800

    re

--- microscope-trace/src/main/java/com/vipshop/microscope/trace/span/LogicPointBuilder.java
+++ microscope-trace/src/main/java/com/vipshop/microscope/trace/span/LogicPointBuilder.java
@@ -4,6 +4,13 @@ import com.vipshop.microscope.thrift.LogicPoint;
 
 public class LogicPointBuilder {
 	
+	public static LogicPoint build(String msg) {
+		LogicPoint logicPoint = new LogicPoint();
+		logicPoint.setMsg("user login operation...");
+		
+		return logicPoint;
+	}
+	
 	public static LogicPoint build() {
 		LogicPoint logicPoint = new LogicPoint();
 		logicPoint.setMsg("user login operation...");
