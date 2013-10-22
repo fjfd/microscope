b0b6f715dde7fafedfc5ec609d06a4938dea5db8
xu fei <andy19881006@gmail.com>
2013-10-22 13:07:57 +0800

    re

--- microscope-trace/src/main/java/com/vipshop/microscope/trace/Tracer.java
+++ microscope-trace/src/main/java/com/vipshop/microscope/trace/Tracer.java
@@ -4,18 +4,38 @@ import com.vipshop.microscope.trace.span.Category;
 
 public class Tracer {
 	
+	/**
+	 * trace start api
+	 * 
+	 * @param spanName
+	 * @param category
+	 */
 	public static void clientSend(String spanName, Category category){
 		TraceFactory.getTrace().clientSend(spanName, category);
 	}
 	
+	/**
+	 * trace end api
+	 */
 	public static void clientReceive() {
 		TraceFactory.getTrace().clientReceive();
 	}
 	
+	/**
+	 * app programmer api
+	 * 
+	 * @param key
+	 * @param value
+	 */
 	public static void record(String key, String value) {
 		TraceFactory.getTrace().record(key, value);
 	}
 	
+	/**
+	 * asyn thread invoke.
+	 * 
+	 * @return
+	 */
 	public static Trace getContext() {
 		return TraceFactory.getContext();
 	}
@@ -24,6 +44,11 @@ public class Tracer {
 		TraceFactory.setContext(trace);
 	}
 	
+	/**
+	 * integrate with other system.
+	 * 
+	 * @return
+	 */
 	public static long getTraceId() {
 		return TraceFactory.getTraceId();
 	}
