d01aa07699adedae1dce40c373d0532ea0ea1954
xu fei <andy19881006@gmail.com>
2013-10-30 11:38:02 +0800

    re

--- microscope-trace/src/test/java/com/vipshop/microscope/trace/CollectorTest.java
+++ microscope-trace/src/test/java/com/vipshop/microscope/trace/CollectorTest.java
@@ -1,25 +1,12 @@
 package com.vipshop.microscope.trace;
 
-import java.util.concurrent.TimeUnit;
-
-import com.vipshop.microscope.trace.span.Category;
+import org.testng.annotations.Test;
 
 public class CollectorTest {
 
-	public static void main(String[] args) throws InterruptedException {
-		
-		Tracer.clientSend("order", Category.ACTION);
+	@Test
+	public void test() {
 		new UserService().login();
-		
-		Tracer.record("url", "http://zipkin.web:8080/trace/queryCondition?callback=%22alfjdsfadfdk%22");
-		Tracer.record("responsecode", "200");
-		Tracer.clientReceive();
-		
-		TimeUnit.SECONDS.sleep(60);
-		
-		System.out.println(" get all trace info from hbase");
-		
-		System.exit(0);
 	}
 
 }
