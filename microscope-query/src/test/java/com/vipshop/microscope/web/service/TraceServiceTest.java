526c2c915e0893e4ef73c5ad0795820830576624
xu fei <andy19881006@gmail.com>
2013-11-06 16:37:23 +0800

    add
    添加测试用例

--- microscope-query/src/test/java/com/vipshop/microscope/web/service/TraceServiceTest.java
+++ microscope-query/src/test/java/com/vipshop/microscope/web/service/TraceServiceTest.java
@@ -1,7 +1,11 @@
 package com.vipshop.microscope.web.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.testng.annotations.Test;
 
+import com.vipshop.microscope.common.util.TimeRangeUtil;
 import com.vipshop.microscope.web.service.TraceService;
 
 public class TraceServiceTest {
@@ -9,11 +13,25 @@ public class TraceServiceTest {
 	TraceService service = new TraceService();
 	
 	@Test
-	public void testGetTraceList() {
-		System.out.println(service.getTraceSpan("-4403393697944183921"));;
-		System.out.println("skfjdl");
+	public void testGetQueryCondition() {
+		System.out.println(service.getQueryCondition());;
 	}
 	
+	@Test
+	public void testGetTraceList() {
+		Map<String, String> query = new HashMap<String, String>();
+		query.put("appName", "picket");
+		query.put("traceName", "order");
+		query.put("startTime", String.valueOf(TimeRangeUtil.dayOfBegin()));
+		query.put("endTime", String.valueOf(TimeRangeUtil.dayOfEnd()));
+		query.put("limit", "100");
+		System.out.println(service.getTraceList(query));;
+	}
+	
+	@Test
+	public void testGetTraceSpan() {
+		System.out.println(service.getTraceSpan("-9113400607919331149"));
+	}
 	
 
 }
