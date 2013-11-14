b195f9463db078b5953110bd1c50ad885d997f44
xu fei <andy19881006@gmail.com>
2013-11-14 18:38:56 +0800

    re

--- microscope-mysql/src/main/java/com/vipshop/microscope/mysql/factory/DaoFactory.java
+++ microscope-mysql/src/main/java/com/vipshop/microscope/mysql/factory/DaoFactory.java
@@ -8,14 +8,14 @@ import com.vipshop.microscope.mysql.dao.TraceReportDao;
 
 public class DaoFactory {
 	
-	public static final TraceReportDao TRACE_REPORT_DAO;
 	public static final MsgReportDao MSG_REPORT_DAO;
+	public static final TraceReportDao TRACE_REPORT_DAO;
 	
 	static {
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("/applicationContext-database.xml", DaoFactory.class);
 		
-		TRACE_REPORT_DAO = context.getBean(TraceReportDao.class);
 		MSG_REPORT_DAO = context.getBean(MsgReportDao.class);
+		TRACE_REPORT_DAO = context.getBean(TraceReportDao.class);
 		
 		context.close();
 	}
