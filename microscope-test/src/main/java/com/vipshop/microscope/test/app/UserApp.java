3b27dd94ac418497f21d46299fe4c64dd5e92108
xu fei <andy19881006@gmail.com>
2013-11-19 17:59:20 +0800

    re

--- microscope-test/src/main/java/com/vipshop/microscope/test/app/UserApp.java
+++ microscope-test/src/main/java/com/vipshop/microscope/test/app/UserApp.java
@@ -1,23 +1,23 @@
 package com.vipshop.microscope.test.app;
 
 import com.vipshop.microscope.collector.server.CollectorServer;
+import com.vipshop.microscope.test.app.httpclient.UserHttpClient;
 import com.vipshop.microscope.test.app.server.TestWebServer;
-import com.vipshop.microscope.test.app.url.UserURL;
 
 public class UserApp {
-	
+
 	private static TestWebServer testWebServer = new TestWebServer(9090);
-	
+
 	public static void main(String[] args) throws Exception {
-		
+
 		new Thread(new CollectorServer()).start();
-		
+
 		testWebServer.start();
-		
-		UserURL userURL = new UserURL();
-		
+
+		UserHttpClient userURL = new UserHttpClient();
+
 		userURL.insertRequest();
-		
-//		TimeUnit.SECONDS.sleep(5);
+
+		// TimeUnit.SECONDS.sleep(5);
 	}
 }
