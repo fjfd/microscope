96239ff971498cc6c9c970820ea0958b21285b1d
xu fei <andy19881006@gmail.com>
2013-11-25 13:28:42 +0800

    re

--- microscope-kafka/src/main/java/com/vipshop/microscope/kafka/client/KafkaClient.java
+++ microscope-kafka/src/main/java/com/vipshop/microscope/kafka/client/KafkaClient.java
@@ -1,5 +1,45 @@
 package com.vipshop.microscope.kafka.client;
 
-public class KafkaClient {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Properties;
 
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+public class KafkaClient {
+	
+	private Producer<String, String> inner;
+
+	public KafkaClient() throws Exception {
+		Properties properties = new Properties();
+		properties.load(ClassLoader.getSystemResourceAsStream("producer.properties"));
+		ProducerConfig config = new ProducerConfig(properties);
+		inner = new Producer<String, String>(config);
+	}
+
+	public void send(String topicName, String message) {
+		if (topicName == null || message == null) {
+			return;
+		}
+		KeyedMessage<String, String> km = new KeyedMessage<String, String>(topicName, message);
+		inner.send(km);
+	}
+
+	public void send(String topicName, Collection<String> messages) {
+		if (topicName == null || messages == null) {
+			return;
+		}
+		if (messages.isEmpty()) {
+			return;
+		}
+		List<KeyedMessage<String, String>> kms = new ArrayList<KeyedMessage<String, String>>();
+		for (String entry : messages) {
+			KeyedMessage<String, String> km = new KeyedMessage<String, String>(topicName, entry);
+			kms.add(km);
+		}
+		inner.send(kms);
+	}
 }
