3dfa9a855a4d11828b6763b4cbdb3fbe79f6ae24
xu fei <andy19881006@gmail.com>
2013-11-26 18:37:18 +0800

    re

--- microscope-framework/src/main/java/com/vipshop/microscope/framework/filter/MicroscopeFilter.java
+++ microscope-framework/src/main/java/com/vipshop/microscope/framework/filter/MicroscopeFilter.java
@@ -8,8 +8,12 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import com.vipshop.microscope.trace.Tracer;
+import com.vipshop.microscope.trace.span.Category;
+import com.vipshop.microscope.trace.span.HTTPHeader;
+import com.vipshop.microscope.trace.span.ResultCode;
 
 /**
  * Use for trace Servlet.
@@ -28,8 +32,19 @@ public class MicroscopeFilter implements Filter{
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		Tracer.cleanContext();
-		chain.doFilter(request, response);
+		String traceId = ((HttpServletRequest) request).getHeader(HTTPHeader.X_B3_TRACE_ID);
+		String spanId = ((HttpServletRequest) request).getHeader(HTTPHeader.X_B3_SPAN_ID);
+		
+		String name = ((HttpServletRequest) request).getPathInfo() + "@" + ((HttpServletRequest) request).getMethod();
+		Tracer.clientSend(traceId, spanId, name, Category.ACTION);
+		try {
+			chain.doFilter(request, response);
+		} catch (Exception e) {
+			Tracer.setResultCode(ResultCode.EXCEPTION);
+		} finally {
+			Tracer.clientReceive();
+		}
+		
 	}
 
 	@Override
