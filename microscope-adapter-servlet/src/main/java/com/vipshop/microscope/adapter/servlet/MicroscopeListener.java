31de3dccacdfaf9aaf3470f546ef69e178d1c5c2
xu fei <alex.xu01@vipshop.com>
2014-01-24 17:14:10 +0800

    maven pulgin, adapter for httpclient, jdbc, soa

---
+++ microscope-adapter-servlet/src/main/java/com/vipshop/microscope/adapter/servlet/MicroscopeListener.java
@@ -0,0 +1,5 @@
+package com.vipshop.microscope.adapter.servlet;
+
+public class MicroscopeListener {
+
+}
---
+++ microscope-maven/src/main/java/com/vipshop/microscope/maven/InstallMojo.java
@@ -0,0 +1,15 @@
+package com.vipshop.microscope.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+public class InstallMojo extends AbstractMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
