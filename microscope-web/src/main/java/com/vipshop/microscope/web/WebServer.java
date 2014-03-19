e64198e80e94bb9f5ec73de981ab0958a20e1332
alex.xu01 <alex.xu01@vipshop.com>
2014-03-19 19:00:29 +0800

    update web api

--- microscope-web/src/main/java/com/vipshop/microscope/web/WebServer.java
+++ microscope-web/src/main/java/com/vipshop/microscope/web/WebServer.java
@@ -3,13 +3,27 @@ package com.vipshop.microscope.web;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 
+import com.vipshop.microscope.query.server.QueryServer;
+
 public class WebServer {
 
 	public static void main(String[] args) throws Exception {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					new QueryServer().start();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		
 		Server server = new Server(80);
 		ResourceHandler resourceHandler = new ResourceHandler();
-		resourceHandler.setResourceBase("src/main/resources");
+		resourceHandler.setResourceBase("E:\\Workspace\\microscope-web");
 		server.setHandler(resourceHandler);
 		server.start();
+		
 	}
 }
--- microscope-web/src/main/resources/js/common/filters.js
+++
@@ -1,9 +0,0 @@
-﻿'use strict';
-
-angular.module('microscope.app.filters', [])
-	.filter('getDateTime', function () {
-	    return function (date) {
-	        var dateObj = new Date(date / 1000);
-	        return dateObj.format('yyyy-MM-dd hh:mm:ss');
-	    }
-	});
