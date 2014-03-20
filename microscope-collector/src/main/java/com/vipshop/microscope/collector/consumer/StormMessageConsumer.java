ba84acb9ac26926732272afdbb0ea6a275cfa6f3
alex.xu01 <alex.xu01@vipshop.com>
2014-03-20 14:40:50 +0800

    update storage api

---
+++ microscope-collector/src/main/java/com/vipshop/microscope/collector/consumer/StormMessageConsumer.java
@@ -0,0 +1,25 @@
+package com.vipshop.microscope.collector.consumer;
+
+import com.vipshop.microscope.common.logentry.LogEntry;
+
+public class StormMessageConsumer implements MessageConsumer {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void publish(LogEntry logEntry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ microscope-collector/src/main/java/com/vipshop/microscope/collector/storm/TraceStorageBolt.java
@@ -0,0 +1,5 @@
+package com.vipshop.microscope.collector.storm;
+
+public class TraceStorageBolt {
+
+}
